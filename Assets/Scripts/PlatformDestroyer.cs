5c965d634af21ac7b9b4d347b880f76a5859406b
DJ <davidjoshua@u.nus.edu>
2016-11-01 10:07:52 +0800

    Fixing Endless running
    # Conflicts:
    #	Assets/Scripts/HungerCtrl.cs
    #	Assets/Scripts/ReHealth.cs
    #	Assets/_Scenes/IriMain.unity
    #	Assets/prefabs/berry.prefab

--- Assets/Scripts/PlatformDestroyer.cs
+++ Assets/Scripts/PlatformDestroyer.cs
@@ -5,14 +5,18 @@ public class PlatformDestroyer : MonoBehaviour {
 
 	public GameObject platformDestructionPoint;
 
+	public bool Important;
+
 	// Use this for initialization
 	void Start () {
 		platformDestructionPoint = GameObject.Find ("Platform Destruction Point");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (transform.position.x < platformDestructionPoint.transform.position.x) {
+	void Update () 
+	{
+		if (transform.position.x < platformDestructionPoint.transform.position.x && !Important) 
+		{
 			Destroy (gameObject);
 			//gameObject.SetActive(false);
 		}
