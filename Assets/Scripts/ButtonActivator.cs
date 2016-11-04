0bd1362c53fd47162f51b3fff693ffbdea65d7f3
DJ <davidjoshua@u.nus.edu>
2016-11-04 16:06:40 +0800

    Lerping Buttons

---
+++ Assets/Scripts/ButtonActivator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonActivator : MonoBehaviour 
+{
+
+	public GameObject ToActivate;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name =="Player") 
+		{
+			ToActivate.SetActive (true);
+		}
+	}
+}
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,21 +7,32 @@ public class UIController : MonoBehaviour {
 
 	public Text lives;
 
+	public Image Pounce, Phase, Bite, Leap;
+
 	[SerializeField]
 	private PlayerController playerController;
 
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 
 		playerController = FindObjectOfType<PlayerController> ();
-		lives.text = "Lives :" + playerController.Lives.ToString();	
+		lives.text = "Lives :" + playerController.Lives.ToString();
+
+		if (playerController.Fenrir) {
+			Pounce.fillAmount = Mathf.Lerp (0, 1, playerController.PounceCD / playerController.PounceCoolDown);
+			Bite.fillAmount = Mathf.Lerp (0, 1, playerController.PounceCD / 1f);
+		}
+
+		if (playerController.Iri) {
+			Phase.fillAmount = Mathf.Lerp (0, 1, playerController.PounceCD / 4f);
+			Leap.fillAmount = Mathf.Lerp (0, 1, playerController.PounceCD / 2f);
+		}
 	}
 
 	public void LoadScene(string SceneName)
 	{
-//		PlayerData.IsKiller = false;
-//		PlayerData.AlignSet = false;
 		SceneManager.LoadScene(SceneName);
 	}
 
