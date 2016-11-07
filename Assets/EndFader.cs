b6b0ea4c27f888820347363b84d726edf6376c39
DJ <davidjoshua@u.nus.edu>
2016-11-07 16:21:01 +0800

    Cleanup of SceneFader, EndFader created for final text

---
+++ Assets/EndFader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndFader : MonoBehaviour {
+
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () 
+	{
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
