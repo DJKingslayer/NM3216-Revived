41dca168908da5dd011c86389d17cabe54ea0a2e
Ogbog21 <lee.wenyi.mark93@gmail.com>
2016-11-07 21:42:21 +0800

    Fixed the Fenrir transition

--- Assets/Scripts/StoryDialogue.cs
+++ Assets/Scripts/StoryDialogue.cs
@@ -91,7 +91,7 @@ public class StoryDialogue : MonoBehaviour {
 
 			if (Input.GetKeyDown (KeyCode.Space))
 			{
-				SceneManager.LoadScene ("End");
+				SceneManager.LoadScene ("DEnd");
 			}
 		}
 
