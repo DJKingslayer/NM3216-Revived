c3c3aa228cc1d9d3368f373cf29869621cdb4d12
DJ <davidjoshua@u.nus.edu>
2016-11-08 08:39:53 +0800

    UI fixes.
    Iri will force end if flower 6 is collected

---
+++ Assets/Scripts/BackupSceneEnd.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BackupSceneEnd : MonoBehaviour {
+
+	public GameObject CheckPresent;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (CheckPresent == null) 
+		{
+			SceneManager.LoadScene ("End");
+		}
+	}
+}
